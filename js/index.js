df13d3f4e2397720f50b679b3e51c4c87c507130
Omar Khan <khancodegt@gmail.com>
2015-04-06 19:19:56 -0400

    added treemap plus viz

--- js/index.js
+++ js/index.js
@@ -1,3 +1,65 @@
 /**
  * Created by khancode on 3/19/2015.
  */
+
+console.log("inside index.js");
+
+var colleges;
+
+///* Omar wrote this */
+//$.getScript("js/treemap_plus.js", function(){
+//
+//    console.log("Script loaded and executed.");
+//    // here you can use anything you defined in the loaded script
+//
+//    setTimeout(function (){
+//
+//        //something you want delayed
+//        setSelector();
+//
+//    }, 1000); // how long do you want the delay to be?
+//
+//});
+//
+//function setSelector() {
+//    $(".d3plus_rect").click(function() {
+//        //alert('hai');
+//
+//        //alert($(this).text());
+//
+//        //var backgroundColor = $(this).css("background-color");
+//        //alert("bgcolor: " + backgroundColor);
+//
+//        //$(this).css('background-color', 'yellow');
+//        //$(this).
+//
+//        //alert($(this).find("tspan").text());
+//        console.log($(this).find("text").attr('id'));
+//        var id = $(this).find("text").attr('id');
+//        var split = id.split('_');
+//        //alert(split);
+//
+//        var college = '';
+//        for (var i = 2; i < split.length - 1; i++) {
+//            college += split[i];
+//
+//            if (i != split.length - 2)
+//                college += ' ';
+//        }
+//
+//        alert(college);
+//        console.log("College selected: " + college);
+//
+//        // now get background color
+//        var bgclr = $(this).find("rect").css('fill');
+//        console.log(bgclr);
+//
+//        $(this).find("rect").css('fill', 'yellow');
+//
+//
+//        data.push({name:college});
+//        rawr.$apply(function() {
+//            rawr.colleges = data;
+//        })
+//    });
+//}
---
+++ js/treemap_plus.js
@@ -0,0 +1,23 @@
+/**
+ * Created by khancode on 3/19/2015.
+ */
+
+// sample data array
+var sample_data = [
+    {"value": 50000, "name": "College of Architecture"},
+    {"value": 77500, "name": "College of Computing"},
+    {"value": 66716, "name": "College of Engineering"},
+    {"value": 54000, "name": "Ivan Allen College"},
+    {"value": 55000, "name": "Scheller College of Business"},
+    {"value": 43250, "name": "College of Sciences"},
+    {"value": 63000, "name": "Multidisciplinary"}
+];
+
+// instantiate d3plus
+var visualization = d3plus.viz()
+    .container("#viz")  // container DIV to hold the visualization
+    .data(sample_data)  // data to use with the visualization
+    .type("tree_map")   // visualization type
+    .id("name")         // key for which our data is unique on
+    .size("value")      // sizing of blocks
+    .draw();             // finally, draw the visualization!
